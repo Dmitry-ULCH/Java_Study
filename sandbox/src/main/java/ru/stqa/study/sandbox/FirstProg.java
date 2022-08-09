617ff5adadc935730c398083b7f9ed05e6254282
Dmitry-ULCH <110170296+Dmitry-ULCH@users.noreply.github.com>
2022-08-09 18:08:21 +0300

    Update FirstProg.java

--- sandbox/src/main/java/ru/stqa/study/sandbox/FirstProg.java
+++ sandbox/src/main/java/ru/stqa/study/sandbox/FirstProg.java
@@ -2,9 +2,9 @@ package ru.stqa.study.sandbox;
 
 public class FirstProg {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
-}	
+  }
 	
